025d1f9337131f656e8821f1eb445635e411b1ba
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-12 23:38:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(array)
-
-
-      array = array.split.reverse.join(" ") 
-
-
+  words = array.split(" ")
+    reversed_array = []
+    words.collect do |word|
+      reversed_array << word.reverse
+    end
+    reversed_array.join(" ")
 end
